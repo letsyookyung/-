311a3e3e552dd140d4202456f5ce6d1d63c38983
이유경 <58244204+letsyookyung@users.noreply.github.com>
2022-02-13 17:21:03 +0900

    Create valid_parentheses.py

---
+++ LeetCode/valid_parentheses.py
@@ -0,0 +1,28 @@
+        s_list = list(s)
+        brackets = { ')':'(',
+                      '}':'{', 
+                      ']':'['}
+        opening = [s for s in s_list if s in brackets.values()]   
+        closing = [s for s in s_list if s in brackets.keys()]
+        # print(opening)
+        # print(closing)
+        # print("")
+        
+        stack =[]
+        for s in s_list[:] :
+            if s in opening :
+                print('#opening:',s)
+                stack.append(s)
+            if s in closing: 
+                print('#closing:',s)
+                if not stack:
+                    return False
+                elif stack.pop() != brackets[s] :
+                    return False
+                else :
+                    continue
+                    
+        if not stack:
+            return True
+        else :
+            return False
