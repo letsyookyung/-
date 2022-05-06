3087f0377a717b30e6bd37446c9841eb3d7e5477
이유경 <58244204+letsyookyung@users.noreply.github.com>
2022-05-06 10:28:56 +0900

    Create reconstruct-iternary.py

---
+++ LeetCode/reconstruct-iternary.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        
+        graph = collections.defaultdict(list)
+        # print(tickets)
+        # print(sorted(tickets))
+        for a, b in sorted(tickets):
+            # print(a,b)
+            graph[a].append(b)
+            
+        print(graph)
+        route = []
+        def dfs(a):
+            while graph[a]:
+                dfs(graph[a].pop(0))
+            print(a)
+            route.append(a)
+        dfs('JFK')
+        print(route)
+        return route[::-1]
+
+
+
+
+defaultdict(<class 'list'>, {'ATL': ['JFK', 'SFO'], 'JFK': ['ATL', 'SFO'], 'SFO': ['ATL']})
+[]
+SFO
+ATL
+SFO
+JFK
+ATL
+JFK
+['SFO', 'ATL', 'SFO', 'JFK', 'ATL', 'JFK']
