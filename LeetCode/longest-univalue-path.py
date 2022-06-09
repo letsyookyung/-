f583e22a9483795dcb17a6b55cd20d7dbe268932
이유경 <58244204+letsyookyung@users.noreply.github.com>
2022-06-09 23:59:38 +0900

    Update longest-univalue-path.py

--- LeetCode/longest-univalue-path.py
+++ LeetCode/longest-univalue-path.py
@@ -1,1 +1,31 @@
 
+class Solution:
+    def longestUnivaluePath(self, root: Optional[TreeNode]) -> int:
+        
+        def dfs(node):
+            nonlocal ans
+            
+            if not node :
+                return 0
+            
+            left = dfs(node.left)
+            if node.left and node.left.val == node.val :
+                left+=1
+            else :
+                left=0
+                
+            right = dfs(node.right)
+            if node.right and node.right.val == node.val :
+                right +=1
+            else :
+                right = 0
+                
+            ans = max(ans, left+right)
+            
+            return max(left, right)
+    
+        ans=0
+        
+        dfs(root)
+        
+        return ans
